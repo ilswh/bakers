ba634169027b526e6c8ef1c49880137887b58f74
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-01 14:31:26 +0000

    Resfresh access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://bakers-drfapi-f4c1c693be28.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
