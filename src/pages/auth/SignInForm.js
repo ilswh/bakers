e70862bdade385205d30ea1da846ce1a97cd00c2
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-09 15:23:08 +0000

    Change picture at login page

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -113,7 +113,7 @@ function SignInForm() {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+          src={"https://plus.unsplash.com/premium_photo-1664273237565-e5a5c6109bc0?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
         />
       </Col>
     </Row>
