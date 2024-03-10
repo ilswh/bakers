7315f78dc5fa5f908b926377b8e055408d709125
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-10 11:32:31 +0000

    Change picture next to signup form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -130,7 +130,7 @@ const SignUpForm = () => {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
+          src={"https://plus.unsplash.com/premium_photo-1664273237774-b611e4a952dc?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
         />
       </Col>
     </Row>
