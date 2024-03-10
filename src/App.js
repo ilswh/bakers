3d0f9bb6e0c12fce9d0aa6d9217f8cb8c5ff723c
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-10 15:00:42 +0000

    Import contact to app & add to navbar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 import NotFound from "./components/NotFound";
+import ContactCreateForm from "./pages/contacts/ContactCreateForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -72,6 +73,11 @@ function App() {
             exact
             path="/profiles/:id/edit"
             render={() => <ProfileEditForm />}
+          />
+          <Route
+            exact
+            path="/contact/create/"
+            render={() => <ContactCreateForm />}
           />
 
           <Route render={() => <NotFound />} />
