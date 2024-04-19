5e871e0b932716196010fed416d6862e0a5184a3
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-04-19 10:44:38 +0000

    Delete unused code

--- src/pages/contacts/ContactsThankYou.js
+++ src/pages/contacts/ContactsThankYou.js
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
-import Alert from "react-bootstrap/Alert";
 import Modal from "react-bootstrap/Modal";
-import { useHistory } from "react-router-dom";
-import appStyles from "../../App.module.css";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useRedirect } from "../../hooks/useRedirect";
 
 
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Container className={appStyles.Content}>{textFields}</Container>
-      </Form>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Your message have been sent!</Modal.Title>
+
+  <Container>
+    <Modal>
+       <Modal.Header>
+        <Modal.Title>Your message have been sent!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Thank you for contacting bakers. We will get back to you asap!
+        Thank you for contacting bakers. We will get back to you asap!
         </Modal.Body></Modal>
-    </Container>
+  </Container>
 
 
-export default ContactThankYou;
+export default ContactsThankYou;
