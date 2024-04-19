49821df25bf56e6f49931064c67466c42cd30689
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-04-19 11:56:02 +0000

    Comment out ContactsThankYou

--- src/pages/contacts/ContactsThankYou.js
+++ src/pages/contacts/ContactsThankYou.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import Container from "react-bootstrap/Container";
-import Modal from "react-bootstrap/Modal";
-
-
-
-  <Container>
-    <Modal>
-       <Modal.Header>
-        <Modal.Title>Your message have been sent!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        Thank you for contacting bakers. We will get back to you asap!
-        </Modal.Body></Modal>
-  </Container>
+// import Container from "react-bootstrap/Container";
+// import Modal from "react-bootstrap/Modal";
 
 
-export default ContactsThankYou;
+
+//   <Container>
+//     <Modal>
+//        <Modal.Header>
+//         <Modal.Title>Your message have been sent!</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//         Thank you for contacting bakers. We will get back to you asap!
+//         </Modal.Body></Modal>
+//   </Container>
+
+
+// export default ContactsThankYou;
